8de86b05dfa090a2bcb72c8aa2f2ca7b03ef7983
Vlad-Ovidiu Lupu <vladovidiulupu@gmail.com>
2015-01-13 22:22:11 +0200

    Intro to Data Science - Finished problem set 5

--- Intro to Data Science/project_5/busiest_hour/busiest_hour_mapper.py
+++ Intro to Data Science/project_5/busiest_hour/busiest_hour_mapper.py
@@ -1,18 +1,38 @@
 import sys
 import string
+import logging
+
+from util import mapper_logfile
+logging.basicConfig(filename=mapper_logfile, format='%(message)s',
+                    level=logging.INFO, filemode='w')
 
 def mapper():
     """
-    Each line in sys.stdin will be a line from a csv file representing the contents
-    of our final Subway-MTA dataset.  For each line, this mapper should return the following:
-    The unit, the ENTRIESn_hourly, the DATEn, and TIMEn columns, separated by tabs.  
-    Example:
+    In this exercise, for each turnstile unit, you will determine the date and time 
+    (in the span of this data set) at which the most people entered through the unit.
+    
+    The input to the mapper will be the final Subway-MTA dataset, the same as
+    in the previous exercise. You can check out the csv and its structure below:
+    https://www.dropbox.com/s/meyki2wl9xfa7yk/turnstile_data_master_with_weather.csv
 
-    R001    100000.0    2011-05-01  01:00:00
+    For each line, the mapper should return the UNIT, ENTRIESn_hourly, DATEn, and 
+    TIMEn columns, separated by tabs. For example:
+    'R001\t100000.0\t2011-05-01\t01:00:00'
+
+    Since you are printing the output of your program, printing a debug 
+    statement will interfere with the operation of the grader. Instead, 
+    use the logging module, which we've configured to log to a file printed 
+    when you click "Test Run". For example:
+    logging.info("My debugging message")
     """
 
-
+    next(sys.stdin)
     for line in sys.stdin:
-        # your code here
+        tokens = line.strip().split(',')
+        unit = tokens[1]
+        entries = tokens[6]
+        date = tokens[2]
+        time = tokens[3]
+        print '\t'.join([unit, entries, date, time])
 
 mapper()
--- Intro to Data Science/project_5/riders_per_station/riders_per_station_reducer.py
+++ Intro to Data Science/project_5/riders_per_station/riders_per_station_reducer.py
@@ -1,19 +1,43 @@
 import sys
+import logging
+
+from util import reducer_logfile
+logging.basicConfig(filename=reducer_logfile, format='%(message)s',
+                    level=logging.INFO, filemode='w')
 
 def reducer():
     '''
-    Given the output of the mapper for this exercise, the reducer should return one row
-    per unit, along with the total number of ENTRIESn_hourly over the course of may. 
-    
-    You can assume that the input to the reducer is sorted by UNIT, such that all rows 
-    corresponding to a particular UNIT are group together.
+    Given the output of the mapper for this exercise, the reducer should PRINT 
+    (not return) one line per UNIT along with the total number of ENTRIESn_hourly 
+    over the course of May (which is the duration of our data), separated by a tab.
+    An example output row from the reducer might look like this: 'R001\t500625.0'
 
+    You can assume that the input to the reducer is sorted such that all rows
+    corresponding to a particular UNIT are grouped together.
+
+    Since you are printing the output of your program, printing a debug 
+    statement will interfere with the operation of the grader. Instead, 
+    use the logging module, which we've configured to log to a file printed 
+    when you click "Test Run". For example:
+    logging.info("My debugging message")
     '''
-    
-    entries = 0
+
+    total_entries = 0
     old_key = None
-
+        
     for line in sys.stdin:
-        # your code here
-
+        tokens = line.strip().split('\t')
+        unit = tokens[0]
+        entries = float(tokens[1])
+        
+        if unit != old_key:
+            if old_key != None:
+                print old_key + '\t' + str(total_entries)
+            total_entries = 0
+            old_key = unit
+        
+        total_entries += entries
+            
+    print unit + '\t' + str(total_entries)
+        
 reducer()
